7e4e3a45485b00f5596daca61205dbba88a20071
northinrtm <northinrtm@gmail.com>
2026-02-13 14:02:56 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.2"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.northinrtm"
+version = "0.0.1-SNAPSHOT"
+description = "REST API для управления клиентами и контактами"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+	runtimeOnly("org.postgresql:postgresql")
+
+	implementation("org.mapstruct:mapstruct:1.6.3")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
